575fc624d9f38c84a031c84521805be6aef5c323
Ao Tanuki <danfeedev@gmail.com>
2018-10-24 17:45:51 -0500

    implement fxml to principal proyect

--- Laboratories/Laboratory3/FIBA_Statistics/src/main/java/view/Controller.java
+++ Laboratories/Laboratory3/FIBA_Statistics/src/main/java/view/Controller.java
@@ -1,5 +1,157 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
 public class Controller {
 
+	 @FXML
+	    private Button buttonImport;
+
+	    @FXML
+	    private ComboBox<?> choose;
+
+	    @FXML
+	    private Label initialValueLabel;
+
+	    @FXML
+	    private TextField valueInitial;
+
+	    @FXML
+	    private TextField finalValue;
+
+	    @FXML
+	    private Label finalValueLabel;
+
+	    @FXML
+	    private RadioButton range;
+
+	    @FXML
+	    private RadioButton noRange;
+
+	    @FXML
+	    private Label value;
+
+	    @FXML
+	    private TextField valuetext;
+
+	    @FXML
+	    private Button buttonSearchList;
+
+	    @FXML
+	    private Button importSpaces;
+
+	    @FXML
+	    private RadioButton defatree;
+
+	    @FXML
+	    private RadioButton noBalance;
+
+	    @FXML
+	    private Label labelName1;
+
+	    @FXML
+	    private Label genderlabel1;
+
+	    @FXML
+	    private Label agelabel1;
+
+	    @FXML
+	    private Label gameslabel1;
+
+	    @FXML
+	    private Label minuteslabel1;
+
+	    @FXML
+	    private Label FieldGoalslabel1;
+
+	    @FXML
+	    private Label ThreePointslabel1;
+
+	    @FXML
+	    private Label Personallabel1;
+
+	    @FXML
+	    private Label PlayerImpactlabel1;
+
+	    @FXML
+	    private TextField nametext1;
+
+	    @FXML
+	    private TextField gendertext1;
+
+	    @FXML
+	    private TextField Agetext1;
+
+	    @FXML
+	    private TextField gamestext1;
+
+	    @FXML
+	    private TextField Minutestext1;
+
+	    @FXML
+	    private TextField FieldGoalstext1;
+
+	    @FXML
+	    private TextField ThreePointstext1;
+
+	    @FXML
+	    private TextField PersonalFoulstext1;
+
+	    @FXML
+	    private TextField PLayerImpacttext1;
+
+	    @FXML
+	    private Label offensiveLabel1;
+
+	    @FXML
+	    private Label TurnOverlabel111;
+
+	    @FXML
+	    private TextField Offensivetext1;
+
+	    @FXML
+	    private TextField TurnOverText1;
+
+	    @FXML
+	    private Button addbutton;
+
+	    @FXML
+	    private Button modifybutton;
+
+	    @FXML
+	    private Button deletebutton;
+
+	    @FXML
+	    private Button buttonSearch;
+
+	    @FXML
+	    void NoBalanceTreeaction(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void accion(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void accionNoRange(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void defaultAction(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void searchList(ActionEvent event) {
+
+	    }
 }
